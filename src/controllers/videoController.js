c2513266b14b24c3f8905e6fe80487056d3f10f8
Kyuhyun Lee <khlee1992@naver.com>
2022-11-01 16:00:47 +0900

    Temp commit (~221031)

---
+++ src/controllers/videoController.js
@@ -0,0 +1,49 @@
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes age",
+        view: 59,
+        id: 0,
+    },
+    {
+        title: "Second Video",
+        rating: 4,
+        comments: 2,
+        createdAt: "1 minutes age",
+        view: 5,
+        id: 1,
+    },
+    {
+        title: "Third Video",
+        rating: 3,
+        comments: 0,
+        createdAt: "0 minutes age",
+        view: 1,
+        id: 2,
+    },
+];
+
+export const trending = (req, res) => {
+    res.render("home", { pageTitle: "Home", videos });
+};
+
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id];
+    res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id];
+    res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos[id].title = title;
+    return res.redirect(`/videos/${id}`);
+};
