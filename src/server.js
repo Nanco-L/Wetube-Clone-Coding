fa20db004ab6008b7a5af06868270f6c76899319
Kyuhyun Lee <khlee1992@naver.com>
2022-12-04 19:48:36 +0900

    bug fix for loading thumbnail

--- src/server.js
+++ src/server.js
@@ -28,11 +28,11 @@ app.use(
 );
 // To solve SharedArrayBuffer error
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
+    // res.setHeader("Access-Control-Allow-Origin", "*");
+    // res.header(
+    //     "Access-Control-Allow-Headers",
+    //     "Origin, X-Requested-With, Content-Type, Accept"
+    // );
     res.header("Cross-Origin-Embedder-Policy", "require-corp");
     res.header("Cross-Origin-Opener-Policy", "same-origin");
     next();
